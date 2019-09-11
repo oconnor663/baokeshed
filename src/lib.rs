c0e9a39aee026488c87d058391d82c3cfb44f7e0
Jack O'Connor <oconnor663@gmail.com>
2019-09-11 11:21:01 -0400

    a ChunkState with no flags yet

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
-use arrayref::array_refs;
+use arrayref::{array_ref, array_refs, mut_array_refs};
+use core::cmp;
 use core::mem::size_of;
 
 type Word = u32;
 type Count = u64;
 
-const WORD_BYTES: usize = size_of::<Word>();
-const WORD_BITS: usize = 8 * WORD_BYTES;
-pub const OUTBYTES: usize = 8 * WORD_BYTES;
-pub const KEYBYTES: usize = 8 * WORD_BYTES;
-pub const BLOCKBYTES: usize = 16 * WORD_BYTES;
+const WORDBYTES: usize = size_of::<Word>();
+const WORDBITS: usize = 8 * WORDBYTES;
+pub const OUTBYTES: usize = 8 * WORDBYTES;
+pub const KEYBYTES: usize = 8 * WORDBYTES;
+pub const BLOCKBYTES: usize = 16 * WORDBYTES;
+pub const CHUNKBYTES: usize = 4096;
 
 const IV: [Word; 8] = [
     0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19,
@@ -25,19 +27,19 @@ const MSG_SCHEDULE: [[usize; 16]; 7] = [
 ];
 
 #[inline(always)]
-fn g(v: &mut [Word; 16], a: usize, b: usize, c: usize, d: usize, x: Word, y: Word) {
-    v[a] = v[a].wrapping_add(v[b]).wrapping_add(x);
-    v[d] = (v[d] ^ v[a]).rotate_right(16);
-    v[c] = v[c].wrapping_add(v[d]);
-    v[b] = (v[b] ^ v[c]).rotate_right(12);
-    v[a] = v[a].wrapping_add(v[b]).wrapping_add(y);
-    v[d] = (v[d] ^ v[a]).rotate_right(8);
-    v[c] = v[c].wrapping_add(v[d]);
-    v[b] = (v[b] ^ v[c]).rotate_right(7);
+fn g(state: &mut [Word; 16], a: usize, b: usize, c: usize, d: usize, x: Word, y: Word) {
+    state[a] = state[a].wrapping_add(state[b]).wrapping_add(x);
+    state[d] = (state[d] ^ state[a]).rotate_right(16);
+    state[c] = state[c].wrapping_add(state[d]);
+    state[b] = (state[b] ^ state[c]).rotate_right(12);
+    state[a] = state[a].wrapping_add(state[b]).wrapping_add(y);
+    state[d] = (state[d] ^ state[a]).rotate_right(8);
+    state[c] = state[c].wrapping_add(state[d]);
+    state[b] = (state[b] ^ state[c]).rotate_right(7);
 }
 
 #[inline(always)]
-fn round(round: usize, msg: &[Word; 16], state: &mut [Word; 16]) {
+fn round(state: &mut [Word; 16], msg: &[Word; 16], round: usize) {
     // Select the message schedule based on the round.
     let schedule = MSG_SCHEDULE[round];
 
@@ -54,36 +56,92 @@ fn round(round: usize, msg: &[Word; 16], state: &mut [Word; 16]) {
     g(state, 3, 4, 9, 14, msg[schedule[14]], msg[schedule[15]]);
 }
 
-#[inline(always)]
 fn compress(
-    state: &mut [Word; 16],
-    msg: &[Word; 16],
+    half_state: &mut [Word; 8],
+    block_words: &[Word; 16],
     count: Count,
-    parent_flag: Word,
-    kdf_flag: Word,
+    block_len: Word,
+    flags: Word,
 ) {
-    // The first half of the state is the chaining value. Overwrite the second
-    // half of the state with new initial values.
-    state[0] = IV[0];
-    state[1] = IV[1];
-    state[2] = IV[2];
-    state[3] = IV[3];
-    state[4] = IV[4] ^ count as Word;
-    state[5] = IV[5] ^ (count >> WORD_BITS) as Word;
-    state[6] = IV[6] ^ parent_flag;
-    state[7] = IV[7] ^ kdf_flag;
+    let mut full_state = [
+        half_state[0],
+        half_state[1],
+        half_state[2],
+        half_state[3],
+        half_state[4],
+        half_state[5],
+        half_state[6],
+        half_state[7],
+        IV[0],
+        IV[1],
+        IV[2],
+        IV[3],
+        IV[4] ^ count as Word,
+        IV[5] ^ (count >> WORDBITS) as Word,
+        IV[6] ^ block_len,
+        IV[7] ^ flags,
+    ];
 
-    round(0, msg, state);
-    round(1, msg, state);
-    round(2, msg, state);
-    round(3, msg, state);
-    round(4, msg, state);
-    round(5, msg, state);
-    round(6, msg, state);
+    round(&mut full_state, block_words, 0);
+    round(&mut full_state, block_words, 1);
+    round(&mut full_state, block_words, 2);
+    round(&mut full_state, block_words, 3);
+    round(&mut full_state, block_words, 4);
+    round(&mut full_state, block_words, 5);
+    round(&mut full_state, block_words, 6);
+
+    half_state[0] ^= full_state[0] ^ full_state[8];
+    half_state[1] ^= full_state[1] ^ full_state[9];
+    half_state[2] ^= full_state[2] ^ full_state[10];
+    half_state[3] ^= full_state[3] ^ full_state[11];
+    half_state[4] ^= full_state[4] ^ full_state[12];
+    half_state[5] ^= full_state[5] ^ full_state[13];
+    half_state[6] ^= full_state[6] ^ full_state[14];
+    half_state[7] ^= full_state[7] ^ full_state[15];
 }
 
-#[inline(always)]
-fn initial_cv(key: &[Word; 8]) -> [Word; 8] {
+fn words_from_bytes(bytes: &[u8; BLOCKBYTES]) -> [Word; 16] {
+    // Parse the message bytes as little endian words.
+    const W: usize = size_of::<Word>();
+    let refs = array_refs!(bytes, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W);
+    [
+        Word::from_le_bytes(*refs.0),
+        Word::from_le_bytes(*refs.1),
+        Word::from_le_bytes(*refs.2),
+        Word::from_le_bytes(*refs.3),
+        Word::from_le_bytes(*refs.4),
+        Word::from_le_bytes(*refs.5),
+        Word::from_le_bytes(*refs.6),
+        Word::from_le_bytes(*refs.7),
+        Word::from_le_bytes(*refs.8),
+        Word::from_le_bytes(*refs.9),
+        Word::from_le_bytes(*refs.10),
+        Word::from_le_bytes(*refs.11),
+        Word::from_le_bytes(*refs.12),
+        Word::from_le_bytes(*refs.13),
+        Word::from_le_bytes(*refs.14),
+        Word::from_le_bytes(*refs.15),
+    ]
+}
+
+fn bytes_from_words(words: &[Word; 8]) -> [u8; OUTBYTES] {
+    let mut bytes = [0; OUTBYTES];
+    {
+        const W: usize = size_of::<Word>();
+        let refs = mut_array_refs!(&mut bytes, W, W, W, W, W, W, W, W);
+        *refs.0 = words[0].to_le_bytes();
+        *refs.1 = words[1].to_le_bytes();
+        *refs.2 = words[2].to_le_bytes();
+        *refs.3 = words[3].to_le_bytes();
+        *refs.4 = words[4].to_le_bytes();
+        *refs.5 = words[5].to_le_bytes();
+        *refs.6 = words[6].to_le_bytes();
+        *refs.7 = words[7].to_le_bytes();
+    }
+    bytes
+}
+
+fn iv(key: &[Word; 8]) -> [Word; 8] {
     [
         IV[0] ^ key[0],
         IV[1] ^ key[1],
@@ -96,43 +154,82 @@ fn initial_cv(key: &[Word; 8]) -> [Word; 8] {
     ]
 }
 
-#[inline(always)]
-fn initial_state(key: &[Word; 8]) -> [Word; 16] {
-    let cv = initial_cv(key);
-    [
-        cv[0], cv[1], cv[2], cv[3], cv[4], cv[5], cv[6], cv[7],
-        // The rest gets set by the compression function.
-        0, 0, 0, 0, 0, 0, 0, 0,
-    ]
+struct ChunkState {
+    half_state: [Word; 8],
+    buf: [u8; BLOCKBYTES],
+    buf_len: u8,
+    count: Count,
 }
 
-#[inline(always)]
-fn hash_parent(
-    left_cv: &[Word; 8],
-    right_cv: &[Word; 8],
-    key: &[Word; 8],
-    kdf_flag: Word,
-) -> [Word; 16] {
-    let msg = [
-        left_cv[0],
-        left_cv[1],
-        left_cv[2],
-        left_cv[3],
-        left_cv[4],
-        left_cv[5],
-        left_cv[6],
-        left_cv[7],
-        right_cv[0],
-        right_cv[1],
-        right_cv[2],
-        right_cv[3],
-        right_cv[4],
-        right_cv[5],
-        right_cv[6],
-        right_cv[7],
-    ];
+impl ChunkState {
+    fn new(key: &[Word; 8], count: Count) -> Self {
+        Self {
+            half_state: iv(key),
+            buf: [0; BLOCKBYTES],
+            buf_len: 0,
+            count,
+        }
+    }
 
-    let mut state = initial_state(key);
-    compress(&mut state, &msg, 0, !0, kdf_flag);
-    state
+    fn fill_buf(&mut self, input: &mut &[u8]) {
+        let take = cmp::min(BLOCKBYTES - self.buf_len as usize, input.len());
+        self.buf[self.buf_len as usize..self.buf_len as usize + take]
+            .copy_from_slice(&input[..take]);
+        self.buf_len += take as u8;
+        *input = &input[take..];
+    }
+
+    fn update(&mut self, mut input: &[u8]) {
+        if self.buf_len > 0 {
+            self.fill_buf(&mut input);
+            if !input.is_empty() {
+                debug_assert_eq!(self.buf_len as usize, BLOCKBYTES);
+                let block_words = words_from_bytes(&self.buf);
+                let flags = 0; // TODO
+                compress(
+                    &mut self.half_state,
+                    &block_words,
+                    self.count,
+                    BLOCKBYTES as Word,
+                    flags,
+                );
+                self.count += BLOCKBYTES as u64;
+                self.buf_len = 0;
+                self.buf = [0; BLOCKBYTES];
+            }
+        }
+
+        while input.len() > BLOCKBYTES {
+            debug_assert_eq!(self.buf_len, 0);
+            let block = array_ref!(input, 0, BLOCKBYTES);
+            let block_words = words_from_bytes(block);
+            let flags = 0; // TODO
+            compress(
+                &mut self.half_state,
+                &block_words,
+                self.count,
+                BLOCKBYTES as Word,
+                flags,
+            );
+            self.count += BLOCKBYTES as u64;
+            input = &input[BLOCKBYTES..];
+        }
+
+        self.fill_buf(&mut input);
+        debug_assert!(input.is_empty());
+    }
+
+    fn finalize(&self) -> [Word; 8] {
+        let mut state_copy = self.half_state;
+        let block_words = words_from_bytes(&self.buf);
+        let flags = 0; // TODO
+        compress(
+            &mut state_copy,
+            &block_words,
+            self.count,
+            self.buf_len as Word,
+            flags,
+        );
+        state_copy
+    }
 }
