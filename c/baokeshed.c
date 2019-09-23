4d173a5f5170ec6788eaf4c03e55eedd733bbe2c
Jack O'Connor <oconnor663@gmail.com>
2019-09-23 10:13:33 -0400

    force inlining in C

--- c/baokeshed.c
+++ c/baokeshed.c
@@ -20,10 +20,14 @@
 #define CHUNK_END 32
 #define CHUNK_START 16
 
-const uint32_t IV[8] = {0x6A09E667UL, 0xBB67AE85UL, 0x3C6EF372UL, 0xA54FF53AUL,
-                        0x510E527FUL, 0x9B05688CUL, 0x1F83D9ABUL, 0x5BE0CD19UL};
+// This isn't portable. Not production code!
+#define INLINE __attribute__((always_inline)) static inline
 
-const uint8_t MSG_SCHEDULE[7][16] = {
+static const uint32_t IV[8] = {0x6A09E667UL, 0xBB67AE85UL, 0x3C6EF372UL,
+                               0xA54FF53AUL, 0x510E527FUL, 0x9B05688CUL,
+                               0x1F83D9ABUL, 0x5BE0CD19UL};
+
+static const uint8_t MSG_SCHEDULE[7][16] = {
     {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
     {14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3},
     {11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4},
@@ -33,13 +37,13 @@ const uint8_t MSG_SCHEDULE[7][16] = {
     {12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11},
 };
 
-static inline uint32_t load32(const void *src) {
+INLINE uint32_t load32(const void *src) {
   const uint8_t *p = (const uint8_t *)src;
   return ((uint32_t)(p[0]) << 0) | ((uint32_t)(p[1]) << 8) |
          ((uint32_t)(p[2]) << 16) | ((uint32_t)(p[3]) << 24);
 }
 
-static inline void store32(void *dst, uint32_t w) {
+INLINE void store32(void *dst, uint32_t w) {
   uint8_t *p = (uint8_t *)dst;
   p[0] = (uint8_t)(w >> 0);
   p[1] = (uint8_t)(w >> 8);
@@ -47,12 +51,12 @@ static inline void store32(void *dst, uint32_t w) {
   p[3] = (uint8_t)(w >> 24);
 }
 
-static inline uint32_t rotr32(uint32_t w, uint32_t c) {
+INLINE uint32_t rotr32(uint32_t w, uint32_t c) {
   return (w >> c) | (w << (32 - c));
 }
 
 // Count the number of 1 bits.
-static inline uint8_t popcnt(uint64_t x) {
+INLINE uint8_t popcnt(uint64_t x) {
   uint8_t count = 0;
   while (x > 0) {
     count += ((uint8_t)x) & 1;
@@ -61,8 +65,8 @@ static inline uint8_t popcnt(uint64_t x) {
   return count;
 }
 
-static inline void g(uint32_t *state, size_t a, size_t b, size_t c, size_t d,
-                     uint32_t x, uint32_t y) {
+INLINE void g(uint32_t *state, size_t a, size_t b, size_t c, size_t d,
+              uint32_t x, uint32_t y) {
   state[a] = state[a] + state[b] + x;
   state[d] = rotr32(state[d] ^ state[a], 16);
   state[c] = state[c] + state[d];
@@ -73,8 +77,7 @@ static inline void g(uint32_t *state, size_t a, size_t b, size_t c, size_t d,
   state[b] = rotr32(state[b] ^ state[c], 7);
 }
 
-static inline void round_fn(uint32_t *state, const uint32_t *msg,
-                            size_t round) {
+INLINE void round_fn(uint32_t *state, const uint32_t *msg, size_t round) {
   // Select the message schedule based on the round.
   const uint8_t *schedule = MSG_SCHEDULE[round];
 
@@ -91,21 +94,20 @@ static inline void round_fn(uint32_t *state, const uint32_t *msg,
   g(state, 3, 4, 9, 14, msg[schedule[14]], msg[schedule[15]]);
 }
 
-static inline uint32_t offset_low(uint64_t offset) { return (uint32_t)offset; }
+INLINE uint32_t offset_low(uint64_t offset) { return (uint32_t)offset; }
 
-static inline uint32_t offset_high(uint64_t offset) {
+INLINE uint32_t offset_high(uint64_t offset) {
   return (uint32_t)(offset >> 32);
 }
 
-static inline uint32_t block_flags(uint8_t block_len, uint8_t internal_flags) {
+INLINE uint32_t block_flags(uint8_t block_len, uint8_t internal_flags) {
   // The lower bits of the block flags word are the block length. The higher
   // bits are the internal flags (ROOT etc.). The middle bits are currently
   // unused.
   return ((uint32_t)block_len) | (((uint32_t)internal_flags) << 24);
 }
 
-static inline void load_msg_words(const uint8_t block[BLOCK_LEN],
-                                  uint32_t words[8]) {
+INLINE void load_msg_words(const uint8_t block[BLOCK_LEN], uint32_t words[8]) {
   words[0] = load32(block + 4 * 0);
   words[1] = load32(block + 4 * 1);
   words[2] = load32(block + 4 * 2);
@@ -124,8 +126,7 @@ static inline void load_msg_words(const uint8_t block[BLOCK_LEN],
   words[15] = load32(block + 4 * 15);
 }
 
-static inline void load_key_words(const uint8_t key[KEY_LEN],
-                                  uint32_t words[8]) {
+INLINE void load_key_words(const uint8_t key[KEY_LEN], uint32_t words[8]) {
   words[0] = load32(key + 4 * 0);
   words[1] = load32(key + 4 * 1);
   words[2] = load32(key + 4 * 2);
@@ -136,8 +137,7 @@ static inline void load_key_words(const uint8_t key[KEY_LEN],
   words[7] = load32(key + 4 * 7);
 }
 
-static inline void write_state_bytes(const uint32_t state[8],
-                                     uint8_t out[OUT_LEN]) {
+INLINE void write_state_bytes(const uint32_t state[8], uint8_t out[OUT_LEN]) {
   store32(&out[4 * 0], state[0]);
   store32(&out[4 * 1], state[1]);
   store32(&out[4 * 2], state[2]);
@@ -146,6 +146,17 @@ static inline void write_state_bytes(const uint32_t state[8],
   store32(&out[4 * 5], state[5]);
   store32(&out[4 * 6], state[6]);
   store32(&out[4 * 7], state[7]);
+}
+
+INLINE void init_iv(const uint32_t key_words[8], uint32_t state[8]) {
+  state[0] = IV[0] ^ key_words[0];
+  state[1] = IV[1] ^ key_words[1];
+  state[2] = IV[2] ^ key_words[2];
+  state[3] = IV[3] ^ key_words[3];
+  state[4] = IV[4] ^ key_words[4];
+  state[5] = IV[5] ^ key_words[5];
+  state[6] = IV[6] ^ key_words[6];
+  state[7] = IV[7] ^ key_words[7];
 }
 
 void compress(uint32_t state[8], const uint8_t block[BLOCK_LEN],
@@ -191,17 +202,6 @@ void compress(uint32_t state[8], const uint8_t block[BLOCK_LEN],
   state[7] = full_state[7] ^ full_state[15];
 }
 
-static inline void init_iv(const uint32_t key_words[8], uint32_t state[8]) {
-  state[0] = IV[0] ^ key_words[0];
-  state[1] = IV[1] ^ key_words[1];
-  state[2] = IV[2] ^ key_words[2];
-  state[3] = IV[3] ^ key_words[3];
-  state[4] = IV[4] ^ key_words[4];
-  state[5] = IV[5] ^ key_words[5];
-  state[6] = IV[6] ^ key_words[6];
-  state[7] = IV[7] ^ key_words[7];
-}
-
 typedef struct {
   uint32_t state[8];
   uint64_t offset;
@@ -219,12 +219,12 @@ void chunk_state_init(chunk_state *self, const uint32_t key_words[8],
   memset(self->buf, 0, BLOCK_LEN);
 }
 
-size_t chunk_state_len(const chunk_state *self) {
+INLINE size_t chunk_state_len(const chunk_state *self) {
   return ((size_t)self->count) + ((size_t)self->buf_len);
 }
 
-size_t chunk_state_fill_buf(chunk_state *self, const uint8_t *input,
-                            size_t input_len) {
+INLINE size_t chunk_state_fill_buf(chunk_state *self, const uint8_t *input,
+                                   size_t input_len) {
   size_t take = BLOCK_LEN - ((size_t)self->buf_len);
   if (take > input_len) {
     take = input_len;
@@ -235,7 +235,7 @@ size_t chunk_state_fill_buf(chunk_state *self, const uint8_t *input,
   return take;
 }
 
-uint8_t chunk_state_maybe_start_flag(const chunk_state *self) {
+INLINE uint8_t chunk_state_maybe_start_flag(const chunk_state *self) {
   if (self->count == 0) {
     return CHUNK_START;
   } else {
@@ -283,9 +283,9 @@ void chunk_state_finalize(const chunk_state *self, uint32_t context,
   write_state_bytes(state_copy, out);
 }
 
-void hash_one_parent(const uint8_t block[BLOCK_LEN],
-                     const uint32_t key_words[8], bool is_root,
-                     uint32_t context, uint8_t out[OUT_LEN]) {
+INLINE void hash_one_parent(const uint8_t block[BLOCK_LEN],
+                            const uint32_t key_words[8], bool is_root,
+                            uint32_t context, uint8_t out[OUT_LEN]) {
   uint8_t flags = PARENT;
   if (is_root) {
     flags |= ROOT;
@@ -311,12 +311,12 @@ void hasher_init(hasher *self, const uint8_t key[KEY_LEN], uint32_t context) {
   self->subtree_hashes_len = 0;
 }
 
-bool hasher_needs_merge(const hasher *self, uint64_t total_bytes) {
+INLINE bool hasher_needs_merge(const hasher *self, uint64_t total_bytes) {
   uint64_t total_chunks = total_bytes / CHUNK_LEN;
   return self->subtree_hashes_len > popcnt(total_chunks);
 }
 
-void hasher_merge_parent(hasher *self) {
+INLINE void hasher_merge_parent(hasher *self) {
   size_t parent_block_start =
       (((size_t)self->subtree_hashes_len) - 2) * OUT_LEN;
   uint8_t out[OUT_LEN];
@@ -326,8 +326,8 @@ void hasher_merge_parent(hasher *self) {
   self->subtree_hashes_len -= 1;
 }
 
-void hasher_push_chunk_hash(hasher *self, uint8_t hash[OUT_LEN],
-                            uint64_t offset) {
+INLINE void hasher_push_chunk_hash(hasher *self, uint8_t hash[OUT_LEN],
+                                   uint64_t offset) {
   assert(self->subtree_hashes_len < MAX_DEPTH);
   while (hasher_needs_merge(self, offset)) {
     hasher_merge_parent(self);
