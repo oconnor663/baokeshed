45584f311831b292ee9bf3f3ce54b6524c29b961
Jack O'Connor <oconnor663@gmail.com>
2019-11-03 01:44:58 -0400

    bench the NEON hasher

--- benches/bench.rs
+++ benches/bench.rs
@@ -160,6 +160,53 @@ fn bench_ffihasher_04_block(b: &mut Bencher) {
 }
 
 #[bench]
+#[cfg(feature = "c_portable")]
+fn bench_ffihasher64_01_long(b: &mut Bencher) {
+    let mut input = RandomInput::new(b, LONG);
+    b.iter(|| {
+        let mut hasher = c64::Hasher::new(&[0; KEY_LEN], 0);
+        hasher.update(input.get());
+        hasher.finalize()
+    });
+}
+
+#[bench]
+#[cfg(feature = "c_portable")]
+fn bench_ffihasher64_02_medium(b: &mut Bencher) {
+    // The C code supports AVX512 and ARM NEON. It also doesn't do any
+    // multithreading. Always use 16 chunks as the "medium" length for
+    // benchmarking C.
+    let mut input = RandomInput::new(b, 16 * CHUNK_LEN);
+    b.iter(|| {
+        let mut hasher = c64::Hasher::new(&[0; KEY_LEN], 0);
+        hasher.update(input.get());
+        hasher.finalize()
+    });
+}
+
+#[bench]
+#[cfg(feature = "c_portable")]
+fn bench_ffihasher64_03_chunk(b: &mut Bencher) {
+    let mut input = RandomInput::new(b, CHUNK_LEN);
+    b.iter(|| {
+        let mut hasher = c64::Hasher::new(&[0; KEY_LEN], 0);
+        hasher.update(input.get());
+        hasher.finalize()
+    });
+}
+
+#[bench]
+#[cfg(feature = "c_portable")]
+fn bench_ffihasher64_04_block(b: &mut Bencher) {
+    let mut input = RandomInput::new(b, baokeshed::portable64::BLOCK_LEN);
+    b.iter(|| {
+        let mut hasher = c64::Hasher::new(&[0; KEY_LEN], 0);
+        hasher.update(input.get());
+        hasher.finalize()
+    });
+}
+
+#[bench]
 fn bench_xof(b: &mut Bencher) {
     let hasher = Hasher::new();
     let mut xof = hasher.finalize_xof();
